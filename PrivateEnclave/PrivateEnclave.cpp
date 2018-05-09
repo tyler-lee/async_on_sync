fe2a784cfaf671159e58969bca0d34df48241db0
Tyler-Lee <lihuorong@iie.ac.cn>
2018-05-09 23:03:23 +0800

    rename encrypt and decrypt

--- PrivateEnclave/PrivateEnclave.cpp
+++ PrivateEnclave/PrivateEnclave.cpp
@@ -36,3 +36,60 @@ void aos_setkey(aos_key_t* key) {
 	return;
 }
 
+//https://wiki.openssl.org/index.php/EVP_Authenticated_Encryption_and_Decryption
+int decrypt_aes_256_gcm(unsigned char *ciphertext, int ciphertext_len, unsigned char *aad,
+	int aad_len, unsigned char *tag, unsigned char *key, unsigned char *iv,
+	int iv_len, unsigned char *plaintext)
+{
+	EVP_CIPHER_CTX *ctx;
+	int len;
+	int plaintext_len;
+	int ret;
+
+	/* Create and initialise the context */
+	if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
+
+	/* Initialise the decryption operation. */
+	if(!EVP_DecryptInit_ex(ctx, EVP_aes_256_gcm(), NULL, NULL, NULL)) handleErrors();
+
+	/* Set IV length. Not necessary if this is 12 bytes (96 bits) */
+	if(!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_IVLEN, iv_len, NULL)) handleErrors();
+
+	/* Initialise key and IV */
+	if(!EVP_DecryptInit_ex(ctx, NULL, NULL, key, iv)) handleErrors();
+
+	/* Provide any AAD data. This can be called zero or more times as
+	 * required
+	 */
+	if(!EVP_DecryptUpdate(ctx, NULL, &len, aad, aad_len)) handleErrors();
+
+	/* Provide the message to be decrypted, and obtain the plaintext output.
+	 * EVP_DecryptUpdate can be called multiple times if necessary
+	 */
+	if(!EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len)) handleErrors();
+	plaintext_len = len;
+
+	/* Set expected tag value. Works in OpenSSL 1.0.1d and later */
+	if(!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_TAG, 16, tag)) handleErrors();
+
+	/* Finalise the decryption. A positive return value indicates success,
+	 * anything else is a failure - the plaintext is not trustworthy.
+	 */
+	ret = EVP_DecryptFinal_ex(ctx, plaintext + len, &len);
+
+	/* Clean up */
+	EVP_CIPHER_CTX_free(ctx);
+
+	if(ret > 0)
+	{
+		/* Success */
+		plaintext_len += len;
+		return plaintext_len;
+	}
+	else
+	{
+		/* Verify failed */
+		return -1;
+	}
+}
+
--- PublicEnclave/PublicEnclave.cpp
+++ PublicEnclave/PublicEnclave.cpp
@@ -31,7 +31,7 @@ void handleErrors(void)
 }
 
 //https://wiki.openssl.org/index.php/EVP_Authenticated_Encryption_and_Decryption
-int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *aad,
+int encrypt_aes_256_gcm(unsigned char *plaintext, int plaintext_len, unsigned char *aad,
 	int aad_len, unsigned char *key, unsigned char *iv, int iv_len,
 	unsigned char *ciphertext, unsigned char *tag)
 {
@@ -77,63 +77,6 @@ int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *aad,
 	return ciphertext_len;
 }
 
-//https://wiki.openssl.org/index.php/EVP_Authenticated_Encryption_and_Decryption
-int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *aad,
-	int aad_len, unsigned char *tag, unsigned char *key, unsigned char *iv,
-	int iv_len, unsigned char *plaintext)
-{
-	EVP_CIPHER_CTX *ctx;
-	int len;
-	int plaintext_len;
-	int ret;
-
-	/* Create and initialise the context */
-	if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
-
-	/* Initialise the decryption operation. */
-	if(!EVP_DecryptInit_ex(ctx, EVP_aes_256_gcm(), NULL, NULL, NULL)) handleErrors();
-
-	/* Set IV length. Not necessary if this is 12 bytes (96 bits) */
-	if(!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_IVLEN, iv_len, NULL)) handleErrors();
-
-	/* Initialise key and IV */
-	if(!EVP_DecryptInit_ex(ctx, NULL, NULL, key, iv)) handleErrors();
-
-	/* Provide any AAD data. This can be called zero or more times as
-	 * required
-	 */
-	if(!EVP_DecryptUpdate(ctx, NULL, &len, aad, aad_len)) handleErrors();
-
-	/* Provide the message to be decrypted, and obtain the plaintext output.
-	 * EVP_DecryptUpdate can be called multiple times if necessary
-	 */
-	if(!EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len)) handleErrors();
-	plaintext_len = len;
-
-	/* Set expected tag value. Works in OpenSSL 1.0.1d and later */
-	if(!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_TAG, 16, tag)) handleErrors();
-
-	/* Finalise the decryption. A positive return value indicates success,
-	 * anything else is a failure - the plaintext is not trustworthy.
-	 */
-	ret = EVP_DecryptFinal_ex(ctx, plaintext + len, &len);
-
-	/* Clean up */
-	EVP_CIPHER_CTX_free(ctx);
-
-	if(ret > 0)
-	{
-		/* Success */
-		plaintext_len += len;
-		return plaintext_len;
-	}
-	else
-	{
-		/* Verify failed */
-		return -1;
-	}
-}
-
 void aos_verify() {
 	EVP_CIPHER_CTX *ctx;
 	if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
@@ -144,83 +87,5 @@ void aos_verify() {
 }
 
 void aos_encrypt(aos_key_t pkey) {
-    AES_KEY aes;
-    unsigned char key[AES_BLOCK_SIZE];        // AES_BLOCK_SIZE = 16
-    unsigned char iv[AES_BLOCK_SIZE];        // init vector
-    unsigned char* encrypt_string;
-    unsigned char* decrypt_string;
-    unsigned int len;        // encrypt length (in multiple of AES_BLOCK_SIZE)
-    unsigned int i;
-
-    // set the encryption length
-    const char* input_string = "Hello World\n今天天气还可以，测试一下在enclave里调用OpenSSL\n";
-    len = ((strlen(input_string)+1) / AES_BLOCK_SIZE + 1) * AES_BLOCK_SIZE;
-
-    // alloc encrypt_string
-    encrypt_string = (unsigned char*)calloc(len, sizeof(unsigned char));
-    if (encrypt_string == NULL) {
-        printf("Unable to allocate memory for encrypt_string\n");
-        //exit(-1);
-    }
-    // alloc decrypt_string
-    decrypt_string = (unsigned char*)calloc(len, sizeof(unsigned char));
-    if (decrypt_string == NULL) {
-        printf("Unable to allocate memory for decrypt_string\n");
-        //exit(-1);
-    }
-
-    // Generate AES 128-bit key
-    for (i=0; i<16; ++i) {
-        key[i] = 32 + i;
-    }
-
-    // Set encryption key
-    for (i=0; i<AES_BLOCK_SIZE; ++i) {
-        iv[i] = 0;
-    }
-#if 1
-    if (AES_set_encrypt_key(key, 128, &aes) < 0) {
-        printf("Unable to set encryption key in AES\n");
-        //exit(-1);
-    }
-    // encrypt (iv will change)
-    AES_cbc_encrypt((unsigned char*)input_string, encrypt_string, len, &aes, iv, AES_ENCRYPT);
-
-#else
-	EVP_CIPHER_CTX *ctx;
-	int outlen;
-	if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
-	//if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, iv)) handleErrors();
-	//if(1 != EVP_EncryptUpdate(ctx, decrypt_string, &outlen, (unsigned char*)input_string, len)) handleErrors();
-	//ciphertext_len = outlen;
-	//if(1 != EVP_EncryptFinal_ex(ctx, decrypt_string + outlen, &outlen)) handleErrors();
-	//ciphertext_len += outlen;
-	EVP_CIPHER_CTX_free(ctx);
-#endif
-
-    // Set decryption key
-    for (i=0; i<AES_BLOCK_SIZE; ++i) {
-        iv[i] = 0;
-    }
-    if (AES_set_decrypt_key(key, 128, &aes) < 0) {
-        printf("Unable to set decryption key in AES\n");
-        //exit(-1);
-    }
-    // decrypt
-    AES_cbc_encrypt(encrypt_string, decrypt_string, len, &aes, iv,
-            AES_DECRYPT);
-
-    // print
-    printf("input_string = %s\n", input_string);
-    printf("encrypted string = ");
-    for (i=0; i<len; ++i) {
-        printf("%02x", encrypt_string[i]);
-    }
-    printf("\n");
-    printf("decrypted string = %s\n", decrypt_string);
-
-
-	free(encrypt_string);
-	free(decrypt_string);
 }
 
